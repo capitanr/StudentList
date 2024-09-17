f7603f61272602fa158df2f25af3bcb6b7803d26
capitanr <capitanr@sheridancollege.ca>
2024-09-17 11:51:21 -0400

    More Fields in Student

--- src/studentlist/Student.java
+++ src/studentlist/Student.java
@@ -7,16 +7,34 @@ package studentlist;
 public class Student {
 
     private String name;
+    private String studentID;
+    private String address;
+
+    public void setStudentID(String studentID) {
+        this.studentID = studentID;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getStudentID() {
+        return studentID;
+    }
+
+    public String getAddress() {
+        return address;
+    }
 
     public Student(String name) {
         this.name = name;
     }
-    
+
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
         this.name = name;
-    } 
+    }
 }
